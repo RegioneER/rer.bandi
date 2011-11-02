74b4bbb8e633502b6aa70b3327aac3e8d716644a
cekk <andrea.cecchi85@gmail.com>
2011-11-02 15:26:55 +0000

    fissato controllo di reinstallazione e log

--- rer/bandi/setuphandlers.py
+++ rer/bandi/setuphandlers.py
@@ -2,7 +2,8 @@
 
 from Products.CMFCore.utils import getToolByName
 from Products.CMFEditions.setuphandlers import DEFAULT_POLICIES
-
+import logging
+logger = logging.getLogger('rer.bandi')
 
 TYPES_TO_VERSION = ('Bando',)
 
@@ -33,10 +34,11 @@ def add_catalog_indexes(context):
     """
     Setup handler to add indexes to the portal catalog
     """
+    if context.readDataFile('rer.bandi_various.txt') is None:
+        return
     portal = context.getSite()
     setup_tool = portal.portal_setup
     setup_tool.runImportStepFromProfile('profile-rer.bandi:default', 'catalog')
-
     catalog = getToolByName(portal, 'portal_catalog')
     indexes = catalog.indexes()
 
@@ -50,16 +52,18 @@ def add_catalog_indexes(context):
     indexables = []
     for idx in wanted:
         if idx[0] in indexes:
-            portal.plone_log("Found the '%s' index in the catalog, nothing changed.\n" % idx[0])
+            logger.info("Found the '%s' index in the catalog, nothing changed.\n" % idx[0])
         else:
             catalog.addIndex(name=idx[0], type=idx[1], extra=idx[2])
-            portal.plone_log("Added '%s' (%s) to the catalog.\n" % (idx[0], idx[1]))
+            logger.info("Added '%s' (%s) to the catalog.\n" % (idx[0], idx[1]))
             indexables.append(idx[0])
     if len(indexables) > 0:
-        portal.plone_log('Indexing new indexes %s.' % ', '.join(indexables))
+        logger.info('Indexing new indexes %s.' % ', '.join(indexables))
         catalog.manage_reindexIndex(ids=indexables)
 
 def addPropertySheet(context):
+    if context.readDataFile('rer.bandi_various.txt') is None:
+        return
     portal = context.getSite()
     DEST=('Cittadini|Cittadini',
           'Imprese|Imprese',
@@ -70,7 +74,7 @@ def addPropertySheet(context):
     rer_bandi_settings = getattr(portal_properties, 'rer_bandi_settings',None)
     if not rer_bandi_settings:
         portal_properties.addPropertySheet(id='rer_bandi_settings',title='RER Bandi settings')
-        portal.plone_log("Added RER Bandi settings property-sheet")
+        logger.info("Added RER Bandi settings property-sheet")
         rer_bandi_settings = getattr(portal_properties, 'rer_bandi_settings',None)
     if not rer_bandi_settings.hasProperty('destinatari_bandi'):
         rer_bandi_settings.manage_addProperty(id='destinatari_bandi',value=DEST,type='lines')
