b45e227f254e2af2364212ab06bbfed1d63551dd
daniele-andreotti <daniele.andreotti@redturtle.it>
2015-10-06 09:09:02 +0200

    Added support for new type of collections

--- rer/bandi/vocabularies.py
+++ rer/bandi/vocabularies.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+from plone.app.vocabularies.catalog import KeywordsVocabulary
 from rer.bandi.filevocabulary import XMLFileVocabulary
 from rer.bandi.psheetvocabulary import PropertySheetVocabulary
 
@@ -7,19 +7,50 @@ from rer.bandi.psheetvocabulary import PropertySheetVocabulary
 TipologiaBandoVocabularyFactory = XMLFileVocabulary(envvar='PLONE_RER_BANDI_VOCAB',
                                                     vocabulary_name='rer.bandi.tipologia.vocabulary',
                                                     default_terms=[
-                                                        ('beni_servizi', 'Acquisizione beni e servizi'),
-                                                        ('agevolazioni', 'Agevolazioni, finanziamenti, contributi'),
+                                                        ('beni_servizi',
+                                                         'Acquisizione beni e servizi'),
+                                                        ('agevolazioni',
+                                                         'Agevolazioni, finanziamenti, contributi'),
                                                         ('altro', 'Altro'),
                                                     ])
 
 
 DestinatariVocabularyFactory = PropertySheetVocabulary(sheet_name='rer_bandi_settings',
                                                        property_name='destinatari_bandi',
-                                                       default_terms = [
-                                                           ('Cittadini', 'Cittadini'),
-                                                           ('Imprese', 'Imprese'),
-                                                           ('Enti locali', 'Enti locali'),
-                                                           ('Associazioni', 'Associazioni'),
+                                                       default_terms=[
+                                                           ('Cittadini',
+                                                            'Cittadini'),
+                                                           ('Imprese',
+                                                            'Imprese'),
+                                                           ('Enti locali',
+                                                            'Enti locali'),
+                                                           ('Associazioni',
+                                                            'Associazioni'),
                                                            ('Altro', 'Altro')
                                                        ])
 
+
+class DestinatariBandoVocabulary(KeywordsVocabulary):
+
+    """
+        Vocabulary factory listing all catalog keywords
+        from the "getDestinatariBando" index
+    """
+
+    keyword_index = 'getDestinatariBando'
+
+
+DestinatariBandoVocabularyFactory = DestinatariBandoVocabulary()
+
+
+class TipologiaBandoVocabulary(KeywordsVocabulary):
+
+    """
+        Vocabulary factory listing all catalog keywords
+        from the "getTipologia_bando" index
+    """
+
+    keyword_index = 'getTipologia_bando'
+
+
+TipologiaBandoVocabularyFactory = TipologiaBandoVocabulary()
