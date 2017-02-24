85eef3891e5477131dbe45ecdf56123a4c1f0059
daniele-andreotti <daniele.andreotti@redturtle.it>
2017-02-24 11:51:31 +0100

    Back to development: 2.2.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ This module contains the tool of rer.bandi
 import os
 from setuptools import setup, find_packages
 
-version = '2.2.4'
+version = '2.2.5.dev0'
 
 tests_require = ['zope.testing', 'Products.PloneTestCase']
 
