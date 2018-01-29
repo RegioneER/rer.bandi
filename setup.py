87d0839a9284ecd450a34aecc6f8cbaa62a5e45d
Andrea Cecchi <andrea.cecchi85@gmail.com>
2018-01-29 11:47:41 +0100

    Preparing release 3.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ This module contains the tool of rer.bandi
 import os
 from setuptools import setup, find_packages
 
-version = '3.0.4.dev0'
+version = '3.0.4'
 
 tests_require = ['zope.testing', 'Products.PloneTestCase']
 
