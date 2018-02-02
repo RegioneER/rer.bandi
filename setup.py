8474a09869d9ee0d58588735b94e384267240446
Andrea Cecchi <andrea.cecchi85@gmail.com>
2018-02-02 14:26:26 +0100

    Back to development: 3.0.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ This module contains the tool of rer.bandi
 import os
 from setuptools import setup, find_packages
 
-version = '3.0.6'
+version = '3.0.7.dev0'
 
 tests_require = ['zope.testing', 'Products.PloneTestCase']
 
