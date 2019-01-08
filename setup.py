57d24ff38a22733075f62d0d93c491ec4a02f158
RedTurtle Technology <sviluppoplone@redturtle.it>
2019-01-08 10:10:19 +0100

    update metadata

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ This module contains the tool of rer.bandi
 import os
 from setuptools import setup, find_packages
 
-version = '3.0.9arpaer'
+version = '3.0.10arpaer'
 
 tests_require = ['zope.testing', 'Products.PloneTestCase']
 
