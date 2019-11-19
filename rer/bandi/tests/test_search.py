6a97cd77a571ee0bf5ba124ecc9be7b447de2828
Giacomo Monari <arsenico13@hotmail.it>
2019-11-19 16:16:00 +0100

    [new][dev] first two tests for the rest route: 'search_bandi_rest'

--- rer/bandi/tests/test_search.py
+++ rer/bandi/tests/test_search.py
@@ -6,15 +6,17 @@ from plone.app.testing import SITE_OWNER_PASSWORD
 from plone.app.testing import TEST_USER_ID
 from plone.restapi.testing import RelativeSession
 from Products.CMFCore.utils import getToolByName
-from rer.bandi.testing import RER_BANDI_INTEGRATION_TESTING
+from rer.bandi.testing import RER_BANDI_API_FUNCTIONAL_TESTING
 
-import unittest
+
 import requests
+import transaction
+import unittest
 
 
 class SearchBandiTest(unittest.TestCase):
 
-    layer = RER_BANDI_INTEGRATION_TESTING
+    layer = RER_BANDI_API_FUNCTIONAL_TESTING
 
     def setUp(self):
         self.app = self.layer["app"]
@@ -30,21 +32,29 @@ class SearchBandiTest(unittest.TestCase):
         self.api_session.auth = (SITE_OWNER_NAME, SITE_OWNER_PASSWORD)
 
         setRoles(self.portal, TEST_USER_ID, ["Manager"])
+
+        # Object creation
         self.bando = api.content.create(
             container=self.portal, type="Bando", title="Bando foo"
         )
+        self.document = api.content.create(
+            container=self.portal, type="Document", title="Empty page"
+        )
 
-    def test_overall_response_format(self):
+        transaction.commit()
+
+    def test_route_exists(self):
         response = self.api_session.get("/@search_bandi_rest")
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.headers.get("Content-Type"), "application/json")
 
+    def test_search_only_bandi(self):
+        response = self.api_session.get("/@search_bandi_rest")
+
         results = response.json()
         self.assertEqual(
-            results[u"items_total"],
-            len(results[u"items"]),
-            "items_total property should match actual item count.",
+            results[u"items_total"], 1,
         )
 
     # def test_search_bando_all(self):
