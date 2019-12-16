42b11295315c70b7f4cdbf233a456dd052698def
Andrea Cecchi <andrea.cecchi85@gmail.com>
2019-12-16 14:49:26 +0100

    update dates handling

--- rer/bandi/browser/static/javascripts/components/BandoItem/index.js
+++ rer/bandi/browser/static/javascripts/components/BandoItem/index.js
@@ -1,6 +1,10 @@
 import React, { useContext } from 'react';
 import { object } from 'prop-types';
-import { format, isPast } from 'date-fns';
+import format from 'date-fns/format';
+import isPast from 'date-fns/isPast';
+import isAfter from 'date-fns/isAfter';
+import isBefore from 'date-fns/isBefore';
+
 import { TranslationsContext } from '../../TranslationsContext';
 
 import './index.less';
@@ -34,27 +38,36 @@ const BandoItem = ({ data }) => {
 
   const { effective, getScadenza_bando } = data;
   const bandoState = calculateState(data);
-  const effectiveDate = effective ? (
-    <p>
-      <span className="labelTB">
-        {getTranslationFor('bandi_published_on', '')}
-      </span>
-      :&nbsp;<span>{format(new Date(effective), 'dd/MM/yyyy')}</span>
-    </p>
-  ) : (
-    ''
-  );
-  const scadenzaBando = getScadenza_bando ? (
-    <p>
-      <span className="labelTB">
-        {getTranslationFor('bando_scadenza_partecipazione', '')}
-      </span>
-      :&nbsp;
-      <span>{format(new Date(getScadenza_bando), 'dd/MM/yyyy HH:mm')}</span>
-    </p>
-  ) : (
-    ''
-  );
+
+  let effectiveDateItem = '';
+  let scadenzaBandoItem = '';
+  if (effective) {
+    const effectiveDate = new Date(effective);
+    if (isAfter(effectiveDate, new Date(1969, 11, 31))) {
+      effectiveDateItem = (
+        <p>
+          <span className="labelTB">
+            {getTranslationFor('bandi_published_on', '')}
+          </span>
+          :&nbsp;<span>{format(new Date(effective), 'dd/MM/yyyy')}</span>
+        </p>
+      );
+    }
+  }
+  if (getScadenza_bando) {
+    const scadenzaDate = new Date(getScadenza_bando);
+    if (isBefore(scadenzaDate, new Date(2100, 11, 31))) {
+      scadenzaBandoItem = (
+        <p>
+          <span className="labelTB">
+            {getTranslationFor('bando_scadenza_partecipazione', '')}
+          </span>
+          :&nbsp;
+          <span>{format(new Date(getScadenza_bando), 'dd/MM/yyyy HH:mm')}</span>
+        </p>
+      );
+    }
+  }
   return (
     <div className="bando-result">
       <h3 className="bandoTitle contenttype-bando">
@@ -71,13 +84,13 @@ const BandoItem = ({ data }) => {
       <div className="bandoDetail">
         {data.description}
         <div className="bandoDates">
-          {effectiveDate}
-          {effectiveDate && scadenzaBando ? (
+          {effectiveDateItem}
+          {effectiveDateItem && scadenzaBandoItem ? (
             <span className="separator">|</span>
           ) : (
             ''
           )}
-          {scadenzaBando}
+          {scadenzaBandoItem}
         </div>
       </div>
     </div>
--- rer/bandi/browser/static/javascripts/index.js
+++ rer/bandi/browser/static/javascripts/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import ProgressBarContainer from './ProgressBarContainer';
 import App from './App';
 
 const rootElement = document.getElementById('bandi-search');
