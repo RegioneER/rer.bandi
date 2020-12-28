342336722e2ad98df6f62951b8dc4d24825d269e
Andrea Cecchi <andrea.cecchi85@gmail.com>
2020-12-28 10:25:28 +0100

    add support for sitesearch custom fields

--- rer/bandi/services/search_parameters/get.py
+++ rer/bandi/services/search_parameters/get.py
@@ -1,11 +1,153 @@
 # -*- coding: utf-8 -*-
+from plone import api
 from plone.restapi.services import Service
+from rer.bandi import _
 from zope.component import getUtility
+from zope.globalrequest import getRequest
 from zope.i18n import translate
 from zope.interface import implementer
 from zope.publisher.interfaces import IPublishTraverse
 from zope.schema.interfaces import IVocabularyFactory
-from rer.bandi import _
+
+
+def getVocabularyTermsForForm(vocab_name, context):
+    """
+    Return the values of vocabulary
+    """
+    utility = getUtility(IVocabularyFactory, vocab_name)
+
+    values = []
+
+    vocab = utility(context)
+
+    for entry in vocab:
+        if entry.title != u"select_label":
+            values.append({"value": entry.value, "label": entry.title})
+    return values
+
+
+def getSearchFields():
+    request = getRequest()
+    portal = api.portal.get()
+
+    return [
+        {
+            "id": "SearchableText",
+            "label": translate(
+                _("bandi_search_text_label", default=u"Search text"),
+                context=request,
+            ),
+            "help": "",
+            "type": "text",
+        },
+        {
+            "id": "stato_bandi",
+            "label": translate(
+                _("bandi_search_state_label", default=u"State"),
+                context=request,
+            ),
+            "help": "",
+            "type": "select",
+            "multivalued": False,
+            "options": [
+                {
+                    "label": translate(
+                        _("bando_state_all_select_label", default="All"),
+                        context=request,
+                    ),
+                    "value": "",
+                },
+                {
+                    "label": translate(
+                        _("bando_state_open_select_label", default="Open"),
+                        context=request,
+                    ),
+                    "value": "open",
+                },
+                {
+                    "label": translate(
+                        _(
+                            "bando_state_inProgress_select_label",
+                            default="In progress",
+                        ),
+                        context=request,
+                    ),
+                    "value": "inProgress",
+                },
+                {
+                    "label": translate(
+                        _(
+                            "bando_state_closed_select_label",
+                            default="Closed",
+                        ),
+                        context=request,
+                    ),
+                    "value": "closed",
+                },
+            ],
+        },
+        {
+            "id": "tipologia_bando",
+            "label": translate(
+                _("bandi_search_type_label", default="Type"), context=request,
+            ),
+            "help": "",
+            "type": "checkbox",
+            "options": getVocabularyTermsForForm(
+                context=portal, vocab_name="rer.bandi.tipologie.vocabulary"
+            ),
+        },
+        {
+            "id": "destinatari",
+            "label": translate(
+                _("destinatari_label", default="Who can apply"),
+                context=request,
+            ),
+            "help": "",
+            "type": "select",
+            "multivalued": True,
+            "options": getVocabularyTermsForForm(
+                context=portal, vocab_name="rer.bandi.destinatari.vocabulary"
+            ),
+        },
+        {
+            "id": "finanziatori",
+            "label": translate(
+                _("finanziatori_label", default="Financed by EU programmes",),
+                context=request,
+            ),
+            "help": "",
+            "type": "select",
+            "multivalued": True,
+            "options": getVocabularyTermsForForm(
+                context=portal, vocab_name="rer.bandi.finanziatori.vocabulary"
+            ),
+        },
+        {
+            "id": "materie",
+            "label": translate(
+                _("materie_label", default="Topic"), context=request
+            ),
+            "help": "",
+            "type": "select",
+            "multivalued": True,
+            "options": getVocabularyTermsForForm(
+                context=portal, vocab_name="rer.bandi.materie.vocabulary"
+            ),
+        },
+        {
+            "id": "Subject",
+            "label": translate(
+                _("subject_label", default="Subjects"), context=request,
+            ),
+            "help": "",
+            "type": "select",
+            "multivalued": True,
+            "options": getVocabularyTermsForForm(
+                context=portal, vocab_name="plone.app.vocabularies.Keywords"
+            ),
+        },
+    ]
 
 
 @implementer(IPublishTraverse)
@@ -14,141 +156,4 @@ class SearchParametersGet(Service):
         super(SearchParametersGet, self).__init__(context, request)
 
     def reply(self):
-        return [
-            {
-                'id': 'SearchableText',
-                'label': translate(
-                    _('bandi_search_text_label', default=u'Search text'),
-                    context=self.request,
-                ),
-                'help': '',
-                'type': 'text',
-            },
-            {
-                'id': 'stato_bandi',
-                'label': translate(
-                    _('bandi_search_state_label', default=u'State'),
-                    context=self.request,
-                ),
-                'help': '',
-                'type': 'select',
-                'multivalued': False,
-                'options': [
-                    {
-                        'label': translate(
-                            _('bando_state_all_select_label', default='All'),
-                            context=self.request,
-                        ),
-                        'value': '',
-                    },
-                    {
-                        'label': translate(
-                            _('bando_state_open_select_label', default='Open'),
-                            context=self.request,
-                        ),
-                        'value': 'open',
-                    },
-                    {
-                        'label': translate(
-                            _(
-                                'bando_state_inProgress_select_label',
-                                default='In progress',
-                            ),
-                            context=self.request,
-                        ),
-                        'value': 'inProgress',
-                    },
-                    {
-                        'label': translate(
-                            _(
-                                'bando_state_closed_select_label',
-                                default='Closed',
-                            ),
-                            context=self.request,
-                        ),
-                        'value': 'closed',
-                    },
-                ],
-            },
-            {
-                'id': 'tipologia_bando',
-                'label': translate(
-                    _('bandi_search_type_label', default='Type'),
-                    context=self.request,
-                ),
-                'help': '',
-                'type': 'checkbox',
-                'options': self.getVocabularyTermsForForm(
-                    'rer.bandi.tipologie.vocabulary'
-                ),
-            },
-            {
-                'id': 'destinatari',
-                'label': translate(
-                    _('destinatari_label', default='Who can apply'),
-                    context=self.request,
-                ),
-                'help': '',
-                'type': 'select',
-                'multivalued': True,
-                'options': self.getVocabularyTermsForForm(
-                    'rer.bandi.destinatari.vocabulary'
-                ),
-            },
-            {
-                'id': 'finanziatori',
-                'label': translate(
-                    _(
-                        'finanziatori_label',
-                        default='Financed by EU programmes',
-                    ),
-                    context=self.request,
-                ),
-                'help': '',
-                'type': 'select',
-                'multivalued': True,
-                'options': self.getVocabularyTermsForForm(
-                    'rer.bandi.finanziatori.vocabulary'
-                ),
-            },
-            {
-                'id': 'materie',
-                'label': translate(
-                    _('materie_label', default='Topic'), context=self.request
-                ),
-                'help': '',
-                'type': 'select',
-                'multivalued': True,
-                'options': self.getVocabularyTermsForForm(
-                    'rer.bandi.materie.vocabulary'
-                ),
-            },
-            {
-                'id': 'Subject',
-                'label': translate(
-                    _('subject_label', default='Subjects'),
-                    context=self.request,
-                ),
-                'help': '',
-                'type': 'select',
-                'multivalued': True,
-                'options': self.getVocabularyTermsForForm(
-                    'plone.app.vocabularies.Keywords'
-                ),
-            },
-        ]
-
-    def getVocabularyTermsForForm(self, vocab_name):
-        """
-        Return the values of vocabulary
-        """
-        utility = getUtility(IVocabularyFactory, vocab_name)
-
-        values = []
-
-        vocab = utility(self.context)
-
-        for entry in vocab:
-            if entry.title != u'select_label':
-                values.append({'value': entry.value, 'label': entry.title})
-        return values
+        return getSearchFields()
