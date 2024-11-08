edf27b291b1fd78d389f6a02af9533446fdf67d2
Daniele Andreotti <daniele.andreotti@redturtle.it>
2024-11-08 14:18:51 +0100

    updated tests

--- rer/bandi/tests/test_collection_criteria.py
+++ rer/bandi/tests/test_collection_criteria.py
@@ -14,40 +14,40 @@ class TestCollectionCriteria(unittest.TestCase):
     layer = RER_BANDI_FUNCTIONAL_TESTING
 
     def setUp(self):
-        self.portal = self.layer['portal']
-        self.request = self.layer['request']
-        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.portal = self.layer["portal"]
+        self.request = self.layer["request"]
+        setRoles(self.portal, TEST_USER_ID, ["Manager"])
         login(self.portal, TEST_USER_NAME)
         self.collection = api.content.create(
-            container=self.portal, type='Collection', title='Collection'
+            container=self.portal, type="Collection", title="Collection"
         )
         self.bando1 = api.content.create(
             container=self.portal,
-            type='Bando',
-            title='Bando with destinatari',
-            destinatari=[u'd1', u'd2', u'dà'],
+            type="Bando",
+            title="Bando with destinatari",
+            destinatari=["d1", "d2", "dà"],
         )
 
         self.bando2 = api.content.create(
             container=self.portal,
-            type='Bando',
-            title='Bando with finanziatori',
-            finanziatori=['f1'],
+            type="Bando",
+            title="Bando with finanziatori",
+            finanziato=True,
         )
 
         self.bando3 = api.content.create(
             container=self.portal,
-            type='Bando',
-            title='Bando with materie',
-            materie=['m1', 'm2'],
+            type="Bando",
+            title="Bando with materie",
+            materie=["m1", "m2"],
         )
 
     def test_query_destinatari(self):
         query = [
             {
-                'i': 'destinatari',
-                'o': 'plone.app.querystring.operation.string.is',
-                'v': ['d1'],
+                "i": "destinatari",
+                "o": "plone.app.querystring.operation.string.is",
+                "v": ["d1"],
             }
         ]
         self.collection.setQuery(query)
@@ -58,9 +58,9 @@ class TestCollectionCriteria(unittest.TestCase):
     def test_query_destinatari_with_accent(self):
         query = [
             {
-                'i': 'destinatari',
-                'o': 'plone.app.querystring.operation.string.is',
-                'v': ['dà'],
+                "i": "destinatari",
+                "o": "plone.app.querystring.operation.string.is",
+                "v": ["dà"],
             }
         ]
         self.collection.setQuery(query)
@@ -71,9 +71,9 @@ class TestCollectionCriteria(unittest.TestCase):
     def test_query_finanziatori(self):
         query = [
             {
-                'i': 'finanziatori',
-                'o': 'plone.app.querystring.operation.string.is',
-                'v': ['f1'],
+                "i": "finanziato",
+                "o": "plone.app.querystring.operation.boolean.isTrue",
+                "v": "",
             }
         ]
         self.collection.setQuery(query)
@@ -84,9 +84,9 @@ class TestCollectionCriteria(unittest.TestCase):
     def test_query_materie(self):
         query = [
             {
-                'i': 'materie',
-                'o': 'plone.app.querystring.operation.string.is',
-                'v': ['m1'],
+                "i": "materie",
+                "o": "plone.app.querystring.operation.string.is",
+                "v": ["m1"],
             }
         ]
         self.collection.setQuery(query)
